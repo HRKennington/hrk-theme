1115284e5fa9e10c7b341c4a04902df5614d7479
JWKennington <jameswkennington@gmail.com>
2019-07-11 12:58:54 -0500

    updates to theme

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@
 """
 
 import setuptools
-import jwk_theme
+import hrk_theme
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(name='jwk_theme',
-                 version=jwk_theme.__version__,
+setuptools.setup(name='hrk_theme',
+                 version=hrk_theme.__version__,
                  description='Pelican theme',
                  long_description=long_description,
                  long_description_content_type="text/markdown",
-                 url=jwk_theme.__github_url__,
+                 url=hrk_theme.__github_url__,
                  author='James W. Kennington',
                  author_email='jameswkennington@gmail.com',
                  license='MIT',
