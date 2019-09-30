8f37407c4d4980b4f0d8195fbef1173a13c78faf
JWKennington <jameswkennington@gmail.com>
2019-09-29 21:06:15 -0500

    add icons

--- hrk_theme/__init__.py
+++ hrk_theme/__init__.py
@@ -5,5 +5,5 @@ __MINOR__ = 1
 __MICRO__ = 3
 __VERSION__ = (__MAJOR__, __MINOR__, __MICRO__)
 __version__ = '.'.join(str(n) for n in __VERSION__)
-__github_url__ = 'http://github.com/JWKennington/jwk-theme'
+__github_url__ = 'http://github.com/HRKennington/hrk-theme'
 THEME_PATH = pathlib.Path(__file__).parent.as_posix()
